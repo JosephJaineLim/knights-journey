547f5d9a0f34b8e5f490ae5cbc168936050ad8b4
Joseph Lim <limjosephjaine@gmail.com>
2023-07-04 02:09:52 +0800

    refactored logic and code

--- knight-journey/script.js
+++ knight-journey/script.js
@@ -1,73 +1,144 @@
-let initialPos = 57;
-let position = initialPos;
-let moveCounter = 0;
-const board = [...new Array(64)].map(e => false);
-board[position] = true;
+let chessBoard = [...new Array(8)].map(el => [...new Array(8)].fill(false));
 
-
+const initialPosistion = "00";
+let position = initialPosistion;
+chessBoard[Number(position.charAt(0))][Number(position.charAt(1))] = true;
 
 const chess = () => {
     let squares = '';
-    
-    //Counter
-    document.getElementById('chess-info').innerHTML = `<h1> Moves: ` + moveCounter + `</h1> <button onclick="restart()">restart</button>`;
+    let k = Number(position.charAt(0));
+    let l =Number(position.charAt(1));
+    isMovePossible = 0;
 
-    for(let i = 0; i < board.length; i++){
-        //Color condition
-        let styleColor;
-        if(board[i]) styleColor = 'background-color: skyblue';
-        
-        //Array checker + Knight Logo
-        let knightIcon = '';
-        if(position == i){
-            knightIcon = `<i class="fa-solid fa-chess-knight" style="font-size: 2.5rem"></i>`;
+    //Arrange Chessboard
+    for(let i = 0; i < 8; i++){
+        for(let j = 0; j < 8; j++){
+
+            let icon = '';
+            if(k === i && l === j){
+                icon = `<i class="fa-solid fa-chess-knight" style="font-size: 2.5rem"></i>`;
+            }
+            
+            let color = '';
+            if(chessBoard[i][j])
+            {
+                color = `background-color: skyblue`;
+            }
+
+            let nextMove = nextMoveChecker(i,j,k,l);
+            if(nextMove !== '') isMovePossible++;
+            
+           squares += `<div class='square' id='${String(i) + String(j)}' style='${color}' onClick='changeSquare(this)'> ${icon + nextMove} </div>` 
         }
-
-        //Next move checker
-        let movePlaceHolder = '';
-        if(Number(position + 6) === i && board[Number(position + 6)] == false || Number(position - 6)  === i && board[Number(position - 6)] == false||Number(position + 10) === i && board[Number(position + 10)] == false || Number(position - 10) === i && board[Number(position - 10)] == false ||Number(position + 15) === i && board[Number(position + 15)] == false || Number(position - 15) === i && board[Number(position -15 )] == false||Number(position + 17) === i && board[Number(position + 17)] == false|| Number(position - 17) === i && board[Number(position - 17)] == false){
-            movePlaceHolder = '<i class="fa-regular fa-circle" style="font-size: 10px"></i>';
-        }
-
-        //square div
-        squares += `<div class='square' id='`+ i +`'style='` + styleColor + `' onclick='change(this)'>` + knightIcon + movePlaceHolder +` </div>`;
     }
 
-    //GameOver checker
-    if (board[position + 6] !== false && board[position - 6] !== false && board[position + 10] !== false && board[position - 10] !== false && board[position + 15] !== false && board[position - 15] !== false && board[position + 17] !== false && board[position - 17] !== false){
-        document.getElementById('chess__gameover').innerHTML += `<h1>Game Over</h1>`;
-    }
-
+    //DOM
+    if(isMovePossible === 0) document.getElementById('chess__gameover').innerHTML = `<h1>Game Over!</h1>`;
     document.getElementById('chess-board').innerHTML = squares;
 }
 
-const change = (ele) => {
-    if(board[ele.id]) 
-    {
-        return;
-    }
+const changeSquare = (e) => {
+    let i = Number(e.id.split('')[0]); // 2
+    let j = Number(e.id.split('')[1]); // 1
 
-    if(Number(position + 6) === Number(ele.id) || Number(position - 6) === Number(ele.id) ||Number(position + 10) === Number(ele.id) || Number(position - 10) === Number(ele.id) ||Number(position + 15) === Number(ele.id) || Number(position - 15) === Number(ele.id) ||Number(position + 17) === Number(ele.id) || Number(position - 17) === Number(ele.id))
+    if(chessBoard[i][j]) return;
+
+    
+    let k = Number(position.charAt(0)); //0
+    let l = Number(position.charAt(1)); //0
+ 
+
+    if(k + 2 === i && l + 1 === j)
     {
-        position = Number(ele.id);
-        board[ele.id] = true;
-        moveCounter++;
-        chess();
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
     }
+    else if(k + 2 === i && l - 1 === j)
+    {
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
+    }
+    else if(k - 2 === i && l + 1 === j)
+    {
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
+    }
+    else if(k - 2 === i && l - 1 === j)
+    {
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
+    }
+    else if(k + 1 === i && l + 2 === j)
+    {
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
+    }
+    else if(k + 1 === i && l - 2 === j)
+    {
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
+    }
+    else if(k - 1 === i && l + 2 === j)
+    {
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
+    }
+    else if(k - 1 === i && l - 2 === j)
+    {
+        chessBoard[i][j] = true;  
+        position = String(i) + String(j);
+    }
+    chess();
+    
 }
 
-
-
 const restart = () => {
-    position = initialPos;
+    position = initialPosistion;
     moveCounter = 0;
-    board.forEach((el,ind) => {
-        if(el === true)
-        {
-            board[ind] = false;
-        }
-    })
-    board[position] = true;
-    document.getElementById('chess__gameover').innerHTML = "";
+    chessBoard = [...new Array(8)].map(el => [...new Array(8)].fill(false));
+    let i = Number(position.charAt(0));
+    let k = Number(position.charAt(1));
+    chessBoard[i][k] = true;
+
+    document.getElementById('chess__gameover').innerHTML = ``;
     chess();
+}
+
+const nextMoveChecker = (i,j,k,l) => {
+    let nextMove = '';
+
+    if(k + 2 < 8 && l + 1 < 8 && chessBoard[k+2][l+1] === false && k + 2 === i && l + 1 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+    else if(k + 2 < 8 && l - 1 >= 0 && chessBoard[k+2][l-1] === false && k + 2 === i && l - 1 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+    else if(k - 2 >= 0 && l + 1 < 8 && chessBoard[k-2][l+1] === false && k - 2 === i && l + 1 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+    else if(k - 2 >= 0 && l - 1 >= 0 && chessBoard[k-2][l-1] === false && k - 2 === i && l - 1 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+    else if(k - 1 >= 0 && l - 2 >= 0 && chessBoard[k-1][l-2] === false && k - 1 === i && l - 2 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+    else if(k - 1 >= 0 && l + 2 < 8 && chessBoard[k-1][l+2] === false && k - 1 === i && l + 2 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+    else if(k + 1 < 8 && l - 2 >= 0 && chessBoard[k+1][l-2] === false && k + 1 === i && l - 2 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+    else if(k + 1 < 8 && l + 2 < 8 && chessBoard[k+1][l+2] === false && k + 1 === i && l + 2 === j)
+    {
+        nextMove = `<i class="fa-regular fa-circle" style="font-size: 10px"></i>`;
+    }
+
+    return nextMove;        
 }
